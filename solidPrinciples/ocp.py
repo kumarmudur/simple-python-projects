b893fd5a95904f83206a59e03af705b6be641f7a
kumarmudur <kumarmudur10@gmail.com>
2025-10-20 19:24:42 +0530

    [solid principles] ocp example changes

--- solidPrinciples/ocp.py
+++ solidPrinciples/ocp.py
@@ -5,6 +5,12 @@ from abc import ABC, abstractmethod
 
 # Classes should be open for extension, but closed for modification.
 
+# 💼 Real-World Project Example
+
+# E-commerce platform:
+# Add new discount types or coupon rules without changing core billing logic — just add new DiscountStrategy subclasses.
+
+
 # ❌ Bad Example
 # 🧨 You must edit this class every time a new discount type appears.
 class Discount:
@@ -25,4 +31,19 @@ class DiscountStrategy(ABC):
     def get_discount(self, price):
         pass
 
+
+class RegularDiscount(DiscountStrategy):
+    def get_discount(self, price):
+        return price * 0.1
+
+
+class VIPDiscount(DiscountStrategy):
+    def get_discount(self, price):
+        return price * 0.2
+
+
+# Extending behavior without modifying existing code
+class SuperVIPDiscount(DiscountStrategy):
+    def get_discount(self, price):
+        return price * 0.3
 
