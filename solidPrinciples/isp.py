264434d2dbb97bb4dd38bc16f28e8436dfe51dc9
kumarmudur <kumarmudur10@gmail.com>
2025-10-22 16:56:02 +0530

    [solid principles] isp example changes

--- solidPrinciples/isp.py
+++ solidPrinciples/isp.py
@@ -1,7 +1,10 @@
 # 4️⃣ Interface Segregation Principle (ISP)
 # Clients should not be forced to depend on interfaces they do not use.
 
+from abc import ABC, abstractmethod
+
 # ❌ Bad Example
+
 
 class Worker:
     def work(self):
@@ -17,3 +20,39 @@ class Robot(Worker):
 
     def eat(self):
         raise NotImplementedError("Robot doesn’t eat")
+
+
+# ✅ Good Example
+class Workable(ABC):
+    @abstractmethod
+    def work(self):
+        pass
+
+
+class Eatable(ABC):
+    @abstractmethod
+    def eat(self):
+        pass
+
+
+class Human(Workable, Eatable):
+    def work(self):
+        print('Human working')
+
+    def eat(self):
+        print("Human eating")
+
+
+class Robot1(Workable):
+    def work(self):
+        print("Robot working")
+
+
+# ➡ Each class implements only what it needs.
+
+# 💼 Real-World Project Example
+# Factory Automation System:
+# Robots only “work,” while Humans “work + eat.”
+# Using segregated interfaces keeps your classes clean.
+
+
